4881938736b1d9d625c61648dcfa4daa26261263
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-21 08:55:22 +1300

    Set up Error component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import {HashRouter as Router, Route} from 'react-dom'
 
 
+import Error from './Error'
+
 const App = () => (
   <div>
+    <Error />
     <h1>Sparrow CMS</h1>
   </div>
 )
