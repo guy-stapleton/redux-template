55489ff304313c696709d3c7f08a3f5512eef7e0
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-25 18:56:38 +1300

    Set up users api route to send back db data

--- server/api/users.js
+++ server/api/users.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const db = require('../db/connection')
 const router = express.Router()
+const {getUsers} = require('../queries/users')
+
 
 router.get('/', (req, res) => {
-  res.json({"message": "I am alive!"})
+  getUsers(db)
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => {
+      res.json(err.message)
+    })
 })
 
 module.exports = router
